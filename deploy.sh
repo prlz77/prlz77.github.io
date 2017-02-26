55267f5615c40349a84175ce60bde3192645a7af
prlz77 <pau.rodri1@gmail.com>
2017-02-26 22:44:18 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 bundle exec jekyll build
 mv _site ../
+git add .
+git commit -m "upload last source"
+git push origin src
 git checkout master
 ls
 echo "These files will be removed, sure? crtl+c to abort"
@@ -7,3 +10,6 @@ read
 rm -r ./*
 cp -rf ../_site/* ./
 rm -r ../_site
+git add .
+git commit -m "deploy"
+git push origin master
